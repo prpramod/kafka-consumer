3afac8c94003228ed5cbee186eba5322e0384be6
pramodpra <pramod.pradhan@ephesoft.com>
2020-05-29 09:21:17 +0530

    Kafka Consumer consuming message

---
+++ src/main/java/com/course/kafkaconsumer/cosumers/HelloKafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.course.kafkaconsumer.cosumers;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+
+
+    @KafkaListener(topics ="t-hello")
+    public void consume(String message){
+
+        System.out.println(message);
+    }
+
+
+
+
+}
